8fe94d217bd1efb23c0a61eaa10376504f4a84b6
dpnpinto <dpnpinto@gmail.com>
2023-03-25 10:22:51 -0100

    git init

---
+++ create_table_alunos.sql
@@ -0,0 +1,5 @@
+CREATE TABLE T_alunos(  
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
+    create_time DATETIME COMMENT 'Create Time',
+    mome VARCHAR(255)
+) COMMENT '';
---
+++ create_table_docentes.sql
@@ -0,0 +1,5 @@
+CREATE TABLE T_Docentes(  
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
+    create_time DATETIME COMMENT 'Create Time',
+    Nome_Docente VARCHAR(50)
+) COMMENT ''
